ae74ad186b944c7f87ef582871d07aa4b58119a4
khaled <khaledwns20@gmail.com>
2025-07-16 13:10:51 +0300

    Version 4

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ console.log("Version 2")
 
 
 console.log("Version 3")
+
+
+console.log("Version 4")
+
+
--- src/script.js
+++ src/script.js
@@ -1,8 +1,11 @@
 console.log("script")
 console.log("Version 1")
 
-console.log("Version 1 updated")
 console.log("Version 2")
 
 
 console.log("version 3")
+
+
+console.log("Script")
+console.log("Version 4")
