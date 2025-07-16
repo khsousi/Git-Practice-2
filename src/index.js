84206732f39d74130cabd568b24e64b70662fd5d
khaled <khaledwns20@gmail.com>
2025-07-16 12:59:18 +0300

    Version 3

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ console.log("index")
 console.log("Version 1")
 console.log("Version 1 updated")
 console.log("Version 2")
+
+
+
+console.log("Version 3")
--- src/script.js
+++ src/script.js
@@ -3,3 +3,6 @@ console.log("Version 1")
 
 console.log("Version 1 updated")
 console.log("Version 2")
+
+
+console.log("version 3")
